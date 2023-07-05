f33a6966da83b68c5b15f11709afb65f890e6adc
Dimitri GAILLOT <dimitri.gaillot@gmail.com>
2023-07-05 15:49:45 +0200

    add Bivariate_cont_cat : need testing

--- projets/projet_5/utils/MVA.py
+++ projets/projet_5/utils/MVA.py
@@ -39,6 +39,9 @@ def all_heatmaps(df):
 
 
 def dendrogram(df):
+    '''
+    Plots a dendrogram from correlation matrix.
+    '''
     from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
     from scipy.spatial.distance import squareform
 
@@ -68,6 +71,52 @@ def pairplot(df, width=10, height=10):
     g = sns.pairplot(df[num_cols])
     g.fig.set_size_inches(width,height)
     plt.show()
+
+
+def Bivariate_cont_cat(data, cont, cat, category):
+    '''
+    Bivariate_cont_cat(data, 'vintage', 'churn', 1)
+    '''
+    #creating 2 samples
+    x1 = data[cont][data[cat]==category][:]
+    x2 = data[cont][~(data[cat]==category)][:]
+
+    #calculating descriptives
+    n1, n2 = x1.shape[0], x2.shape[0]
+    m1, m2 = x1.mean(), x2.mean()
+    std1, std2 = x1.std(), x2.mean()
+
+    #calculating p-values
+    t_p_val = TwoSampT(m1, m2, std1, std2, n1, n2)
+    z_p_val = TwoSampZ(m1, m2, std1, std2, n1, n2)
+
+    #table
+    table = pd.pivot_table(data=data, values=cont, columns=cat, aggfunc = np.mean)
+
+    #plotting
+    plt.figure(figsize = (20,4), dpi=140)
+
+    #barplot
+    plt.subplot(1,3,1)
+    sns.barplot([str(category),'not {}'.format(category)], [m1, m2])
+    plt.ylabel('mean {}'.format(cont))
+    plt.xlabel(cat)
+    plt.title('t-test p-value = {} \n z-test p-value = {}\n {}'.format(t_p_val,
+                                                                z_p_val,
+                                                                table))
+
+    # category-wise distribution
+    plt.subplot(1,3,2)
+    sns.kdeplot(x1, shade= True, color='blue', label = 'churned')
+    sns.kdeplot(x2, shade= False, color='green', label = 'not churned', linewidth = 1)
+    plt.title('categorical distribution')
+
+    # boxplot
+    plt.subplot(1,3,3)
+    sns.boxplot(x=cat, y=cont, data=data)
+    plt.title('categorical boxplot')
+
+
 
 def ping():
     """
