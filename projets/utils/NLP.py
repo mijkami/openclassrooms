89db67c11b52d764a4094fa2ef5c52d940c6ee0e
mijkami <dimitri.gaillot@gmail.com>
2024-04-23 16:53:33 +0200

    add countvectorizer tfidf params, instances and ARI

---
+++ projets/utils/NLP.py
@@ -0,0 +1,102 @@
+import numpy as np
+from sklearn import manifold
+from sklearn import cluster, metrics
+
+import nltk
+nltk.download("stopwords")
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import time
+
+# Tokenizer
+def tokenizer_fct(sentence) :
+    # print(sentence)
+    sentence_clean = sentence.replace('-', ' ').replace('+', ' ').replace('/', ' ').replace('#', ' ')
+    word_tokens = word_tokenize(sentence_clean)
+    return word_tokens
+
+# Stop words
+stop_w = list(set(stopwords.words('english'))) + ['[', ']', ',', '.', ':', '?', '(', ')']
+
+def stop_word_filter_fct(list_words) :
+    filtered_w = [w for w in list_words if not w in stop_w]
+    filtered_w2 = [w for w in filtered_w if len(w) > 2]
+    return filtered_w2
+
+# lower case et alpha
+def lower_start_fct(list_words) :
+    lw = [w.lower() for w in list_words if (not w.startswith("@")) 
+    #                                   and (not w.startswith("#"))
+                                       and (not w.startswith("http"))]
+    return lw
+
+# Lemmatizer (base d'un mot)
+def lemma_fct(list_words) :
+    lemmatizer = WordNetLemmatizer()
+    lem_w = [lemmatizer.lemmatize(w) for w in list_words]
+    return lem_w
+
+# Fonction de préparation du texte pour le bag of words (Countvectorizer et Tf_idf, Word2Vec)
+def transform_bow_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+    sw = stop_word_filter_fct(word_tokens)
+    lw = lower_start_fct(sw)
+    # lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(lw)
+    return transf_desc_text
+
+# Fonction de préparation du texte pour le bag of words avec lemmatization
+def transform_bow_lem_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+    sw = stop_word_filter_fct(word_tokens)
+    lw = lower_start_fct(sw)
+    lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(lem_w)
+    return transf_desc_text
+
+# Fonction de préparation du texte pour le Deep learning (USE et BERT)
+def transform_dl_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+#    sw = stop_word_filter_fct(word_tokens)
+    lw = lower_start_fct(word_tokens)
+    # lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(lw)
+    return transf_desc_text
+
+
+
+# Calcul Tsne, détermination des clusters et calcul ARI entre vrais catégorie et n° de clusters
+def ARI_fct(features, labels, y_cat_num) :
+    time1 = time.time()
+    num_labels=len(labels)
+    tsne = manifold.TSNE(n_components=2, perplexity=30, n_iter=2000, 
+                                 init='random', learning_rate=200, random_state=42)
+    X_tsne = tsne.fit_transform(features)
+    
+    # Détermination des clusters à partir des données après Tsne 
+    cls = cluster.KMeans(n_clusters=num_labels, n_init=100, random_state=42)
+    cls.fit(X_tsne)
+    ARI = np.round(metrics.adjusted_rand_score(y_cat_num, cls.labels_),4)
+    time2 = np.round(time.time() - time1,0)
+    print("ARI : ", ARI, "time : ", time2)
+    
+    return ARI, X_tsne, cls.labels_
+
+
+# visualisation du Tsne selon les vraies catégories et selon les clusters
+def TSNE_visu_fct(X_tsne, y_cat_num, labels, lab_cat, ARI) :
+    fig = plt.figure(figsize=(15,6))
+    
+    ax = fig.add_subplot(121)
+    scatter = ax.scatter(X_tsne[:,0],X_tsne[:,1], c=y_cat_num, cmap='Set1')
+    ax.legend(handles=scatter.legend_elements()[0], labels=lab_cat, loc="best", title="Categorie")
+    plt.title('Représentation des tweets par catégories réelles')
+    
+    ax = fig.add_subplot(122)
+    scatter = ax.scatter(X_tsne[:,0],X_tsne[:,1], c=labels, cmap='Set1')
+    ax.legend(handles=scatter.legend_elements()[0], labels=set(labels), loc="best", title="Clusters")
+    plt.title('Représentation des tweets par clusters')
+    
+    plt.show()
+    print("ARI : ", ARI)
