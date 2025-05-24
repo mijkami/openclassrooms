28bddcfd3d321404281fe93f6c05c63ff46241ca
Mijka <dimitri.gaillot@gmail.com>
2025-05-24 17:52:45 +0400

    update reset to separate query data from function

--- projets/projet_7/app_API/tests/test_main.py
+++ projets/projet_7/app_API/tests/test_main.py
@@ -7,6 +7,46 @@ client = TestClient(app)
 
 VALID_SK_ID_CURR = 442623
 
+query_data = {
+    "EXT_SOURCE_2": 0.5,
+    "EXT_SOURCE_3": 0.5,
+    "PAYMENT_RATE": 0.5,
+    "INSTAL_DPD_MEAN": 0.5,
+    "INSTAL_AMT_PAYMENT_SUM": 0.5,
+    "AMT_ANNUITY": 0.5,
+    "DAYS_BIRTH": -10000,
+    "CODE_GENDER": 1,
+    "ANNUITY_INCOME_PERC": 0.5,
+    "APPROVED_AMT_DOWN_PAYMENT_MAX": 0.5,
+    "DAYS_EMPLOYED_PERC": 0.5,
+    "BURO_DAYS_CREDIT_MEAN": 0.5,
+    "PREV_CNT_PAYMENT_MEAN": 0.5,
+    "AMT_GOODS_PRICE": 0.5,
+    "BURO_AMT_CREDIT_SUM_DEBT_MEAN": 0.5,
+    "ACTIVE_DAYS_CREDIT_MAX": 0.5,
+    "ACTIVE_DAYS_CREDIT_ENDDATE_MEAN": 0.5,
+    "PREV_APP_CREDIT_PERC_MEAN": 0.5,
+    "INSTAL_PAYMENT_PERC_MEAN": 0.5,
+    "NAME_EDUCATION_TYPE_Highereducation": 1,
+    "DAYS_ID_PUBLISH": -1000,
+    "APPROVED_CNT_PAYMENT_MEAN": 0.5,
+    "BURO_DAYS_CREDIT_MAX": 0.5,
+    "INSTAL_AMT_PAYMENT_MIN": 0.5,
+    "INSTAL_DAYS_ENTRY_PAYMENT_SUM": 0.5,
+    "POS_MONTHS_BALANCE_SIZE": 0.5,
+    "PREV_NAME_CONTRACT_STATUS_Refused_MEAN": 0.5,
+    "DAYS_REGISTRATION": -1000,
+    "PREV_APP_CREDIT_PERC_MIN": 0.5,
+    "APPROVED_DAYS_DECISION_MIN": 0.5,
+    "INSTAL_AMT_PAYMENT_MEAN": 0.5,
+    "INSTAL_DBD_SUM": 0.5,
+    "INSTAL_AMT_INSTALMENT_MAX": 0.5,
+    "INCOME_CREDIT_PERC": 0.5,
+    "INSTAL_DAYS_ENTRY_PAYMENT_MEAN": 0.5,
+    "INSTAL_PAYMENT_DIFF_MEAN": 0.5
+}
+
+
 # Test pour l'endpoint /data
 def test_get_data_parquet():
     response = client.get("/data?format=parquet")
@@ -75,47 +115,11 @@ def test_get_shap_not_found():
     response = client.post("/shap", json={"SK_ID_CURR": 99999})
     assert response.status_code == 200
     assert response.json() == {"error": "404: User not found"}
+    
+
 
 # Test pour l'endpoint /predict
 def test_predict():
-    query_data = {
-        "EXT_SOURCE_2": 0.5,
-        "EXT_SOURCE_3": 0.5,
-        "PAYMENT_RATE": 0.5,
-        "INSTAL_DPD_MEAN": 0.5,
-        "INSTAL_AMT_PAYMENT_SUM": 0.5,
-        "AMT_ANNUITY": 0.5,
-        "DAYS_BIRTH": -10000,
-        "CODE_GENDER": 1,
-        "ANNUITY_INCOME_PERC": 0.5,
-        "APPROVED_AMT_DOWN_PAYMENT_MAX": 0.5,
-        "DAYS_EMPLOYED_PERC": 0.5,
-        "BURO_DAYS_CREDIT_MEAN": 0.5,
-        "PREV_CNT_PAYMENT_MEAN": 0.5,
-        "AMT_GOODS_PRICE": 0.5,
-        "BURO_AMT_CREDIT_SUM_DEBT_MEAN": 0.5,
-        "ACTIVE_DAYS_CREDIT_MAX": 0.5,
-        "ACTIVE_DAYS_CREDIT_ENDDATE_MEAN": 0.5,
-        "PREV_APP_CREDIT_PERC_MEAN": 0.5,
-        "INSTAL_PAYMENT_PERC_MEAN": 0.5,
-        "NAME_EDUCATION_TYPE_Highereducation": 1,
-        "DAYS_ID_PUBLISH": -1000,
-        "APPROVED_CNT_PAYMENT_MEAN": 0.5,
-        "BURO_DAYS_CREDIT_MAX": 0.5,
-        "INSTAL_AMT_PAYMENT_MIN": 0.5,
-        "INSTAL_DAYS_ENTRY_PAYMENT_SUM": 0.5,
-        "POS_MONTHS_BALANCE_SIZE": 0.5,
-        "PREV_NAME_CONTRACT_STATUS_Refused_MEAN": 0.5,
-        "DAYS_REGISTRATION": -1000,
-        "PREV_APP_CREDIT_PERC_MIN": 0.5,
-        "APPROVED_DAYS_DECISION_MIN": 0.5,
-        "INSTAL_AMT_PAYMENT_MEAN": 0.5,
-        "INSTAL_DBD_SUM": 0.5,
-        "INSTAL_AMT_INSTALMENT_MAX": 0.5,
-        "INCOME_CREDIT_PERC": 0.5,
-        "INSTAL_DAYS_ENTRY_PAYMENT_MEAN": 0.5,
-        "INSTAL_PAYMENT_DIFF_MEAN": 0.5
-    }
     response = client.post("/predict", json=query_data)
     assert response.status_code == 200
     response_data = response.json()
